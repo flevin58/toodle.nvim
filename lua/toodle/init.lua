cb55bb2e0c3658859d2a81005baa08f1e50c8901
Fernando Levin <flevin58@gmail.com>
2025-10-19 23:30:28 +0200

    refactored functions

--- lua/toodle/init.lua
+++ lua/toodle/init.lua
@@ -1,32 +1,40 @@
 local M = {}
 
-local function map_data(file, dir, data)
-	local full_path = dir .. "/" .. file
+-- --------------------------------------------------------------
+-- Reads a file in a given base folder and maps all occurrences
+-- of "todo"s storing the filename together with line and column
+-- --------------------------------------------------------------
+local function map_todos_in_file(folder, filename, list)
+	local full_path = folder .. "/" .. filename
 	local lines = vim.fn.readfile(full_path)
-	for index, value in ipairs(lines) do
-		local find = string.find(string.lower(value), "table")
-		if find then
-			table.insert(data, {
-				path = full_path,
-				file_name = file,
-				col_idx = find,
-				row = index,
+	for row, value in ipairs(lines) do
+		local col = string.find(string.lower(value), "todo")
+		if col then
+			table.insert(list, {
+				file_path = full_path,
+				file_name = filename,
+				col = col,
+				row = row,
 			})
 		end
 	end
 end
 
--- TODO: asdfasdfasdf
-local function get_files(dir)
+-- --------------------------------------------------------------
+-- Reads recursively the given folder and for each file having a
+-- "todo" string it adds its name, row and column to a table
+-- which is then returned to the calling function
+-- --------------------------------------------------------------
+local function get_files(folder)
 	local return_files = {}
-	for _, file in ipairs(vim.fn.readdir(dir)) do
+	for _, file in ipairs(vim.fn.readdir(folder)) do
 		if vim.startswith(file, ".") then
 			goto continue
 		end
-		local full_path = dir .. "/" .. file
+		local full_path = folder .. "/" .. file
 		if vim.fn.isdirectory(full_path) == 0 then
-			table.insert(return_files, file)
-			map_data(file, dir, return_files)
+			-- table.insert(return_files, file)
+			map_todos_in_file(folder, file, return_files)
 		else
 			for _, f in ipairs(get_files(full_path)) do
 				table.insert(return_files, f)
@@ -43,7 +51,9 @@ function M.setup()
 		local files = get_files(vim.fn.getcwd())
 
 		-- Create a temporary buffer that cannot be saved (false)
+		local current_buf = vim.api.nvim_get_current_buf()
 		local buf = vim.api.nvim_create_buf(false, true)
+		-- TODO
 
 		local lines = {}
 		for _, value in ipairs(files) do
@@ -53,6 +63,16 @@ function M.setup()
 		local win = vim.api.nvim_open_win(buf, true, {
 			split = "right",
 		})
+
+		-- Add bindings so that 'gg' opens the file at the TODO location
+		vim.keymap.set("n", "gg", function()
+			local pos = vim.api.nvim_win_get_cursor(0)
+			local selected = files[pos[1]]
+			vim.api.nvim_win_close(win, false)
+			vim.api.nvim.nvim_get_current_buf(current_buf)
+			vim.api.nvim_command("edit " .. selected.path)
+			vim.api.nvim_win_set_cursor(0, { selected.row, selected.col_idx })
+		end, { buffer = buf })
 
 		-- Add bindings so that 'q' closes the toodle window
 		vim.keymap.set("n", "q", function()
