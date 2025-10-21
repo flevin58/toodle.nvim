713c40b54454d61ff1001d45a0f8e709142b6a1e
Fernando Levin <flevin58@gmail.com>
2025-10-21 23:56:32 +0200

    Added debug code

--- lua/toodle/init.lua
+++ lua/toodle/init.lua
@@ -62,7 +62,7 @@ function M.setup()
 		-- Fill the buffer with debug info
 		local lines = {}
 		for _, entry in pairs(entries) do
-			local entry_line = string.format("%s (%d)", entry.file_name, entry.row)
+			local entry_line = string.format("%s (%d:%d)", entry.file_name, entry.row, entry.start)
 			table.insert(lines, entry_line)
 		end
 		vim.api.nvim_buf_set_lines(buf, 0, -1, true, lines)
