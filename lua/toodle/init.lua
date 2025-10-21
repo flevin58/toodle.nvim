f0d08f15dca9c6ef58bee54d3383b5777eda53aa
Fernando Levin <flevin58@gmail.com>
2025-10-22 01:58:37 +0200

    Added debug code

--- lua/toodle/init.lua
+++ lua/toodle/init.lua
@@ -69,7 +69,7 @@ function M.setup()
 			local pos = vim.api.nvim_win_get_cursor(0)
 			local selected = entries[pos[1]]
 			vim.api.nvim_win_close(win, false)
-			vim.api.nvim.nvim_set_current_buf(current_buf)
+			vim.api.nvim_set_current_buf(current_buf)
 			vim.api.nvim_cmd(vim.api.keyset.edit, selected.file_path)
 			-- vim.api.nvim_command("edit " .. selected.file_path)
 			-- vim.api.nvim_win_set_cursor(0, { selected.row, selecte:wind.col })
