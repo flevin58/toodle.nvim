ba6ef6235a79914d34e8e52613bdba8a9ceb084a
Fernando Levin <flevin58@gmail.com>
2025-10-22 02:15:16 +0200

    Added debug code

--- lua/toodle/init.lua
+++ lua/toodle/init.lua
@@ -72,7 +72,7 @@ function M.setup()
 			vim.api.nvim_set_current_buf(current_buf)
 			-- vim.api.nvim_cmd(vim.api.keyset.edit, selected.file_path)
 			vim.api.nvim_command("edit " .. selected.file_path)
-			-- vim.api.nvim_win_set_cursor(0, { selected.row, selecte:wind.col })
+			vim.api.nvim_win_set_cursor(0, { selected.row, selected.col })
 		end, { buffer = buf })
 
 		-- Add bindings so that 'q' closes the toodle window
