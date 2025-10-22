f32c7764c11c4f87aac60532358ced73081a2567
Fernando Levin <flevin58@gmail.com>
2025-10-22 03:03:59 +0200

    Added debug code

--- lua/toodle/init.lua
+++ lua/toodle/init.lua
@@ -8,7 +8,7 @@ local function map_todos_in_file(folder, file_name, entries)
 	local full_path = folder .. "/" .. file_name
 	local lines = vim.fn.readfile(full_path)
 	for row, value in ipairs(lines) do
-		local col = string.find(value, "PAPPA", 1, true)
+		local col = string.find(value, "TODO", 1, true)
 		if col then
 			table.insert(entries, {
 				file_path = full_path,
